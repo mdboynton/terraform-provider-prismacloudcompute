5518102e37cc32bae8e893cd01d8d849a4339d64
Max Boynton <mboynton@paloaltonetworks.com>
2025-03-31 11:29:23 -0500

    remove convert and sdkv2 directories (old code)

--- internal/convert/string.go
+++
@@ -1,9 +0,0 @@
-package convert
-
-func SchemaToStringSlice(in []interface{}) []string {
-	ans := make([]string, 0, len(in))
-	for _, val := range in {
-		ans = append(ans, val.(string))
-	}
-	return ans
-}
