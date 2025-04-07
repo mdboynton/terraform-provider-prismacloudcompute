a0713d56908ee0c7e63dc44c5b81c1ce594ce5c6
mdboynton <137207294+mdboynton@users.noreply.github.com>
2025-04-07 12:05:15 -0500

    Acceptance Test Action (#28)
    * init commit
    
    * update client_test to work with environment variables
    
    * cleanup actions
    
    * fix password secret reference
    
    * test aws security group update
    
    * add region to sg ingress update calls
    
    * switch SG update commands to use group id instead of group name
    
    * add step to configure aws cli to shorten SG modification calls
    
    * pipe output of aws cli commands to /dev/null
    
    * scope acceptance test action to migrate-to-plugin-framework commits/PRs

--- internal/acceptance/client_test.go
+++ internal/acceptance/client_test.go
@@ -1,8 +1,8 @@
 package acceptance 
 
 import (
-	"encoding/json"
-	"io"
+	//"encoding/json"
+	//"io"
 	"os"
 	"testing"
 	
@@ -10,30 +10,44 @@ import (
 )
 
 func TestAPIClient(t *testing.T) {
-    // Open credentials file
-	credsFile, err := os.Open(credentialsFilePath)
-	if err != nil {
-		t.Errorf("Error opening credentials file: %v", err)
-        return 
-	}
-	defer credsFile.Close()
+    //// Open credentials file
+	//credsFile, err := os.Open(credentialsFilePath)
+	//if err != nil {
+	//	t.Errorf("Error opening credentials file: %v", err)
+    //    return 
+	//}
+	//defer credsFile.Close()
 
-    // Read credentials file contents
-	fileContent, err := io.ReadAll(credsFile)
-	if err != nil {
-		t.Errorf("Error reading credentials file: %v", err)
-		return
-	}
+    //// Read credentials file contents
+	//fileContent, err := io.ReadAll(credsFile)
+	//if err != nil {
+	//	t.Errorf("Error reading credentials file: %v", err)
+	//	return
+	//}
 
-    // Unmarshal contents to config object
-	var config api.PrismaCloudComputeAPIClientConfig
-	if err := json.Unmarshal(fileContent, &config); err != nil {
-		t.Errorf("Error unmarshalling contents of credentials file: %v", err)
-		return
-	}
+    //// Unmarshal contents to config object
+	//var config api.PrismaCloudComputeAPIClientConfig
+	//if err := json.Unmarshal(fileContent, &config); err != nil {
+	//	t.Errorf("Error unmarshalling contents of credentials file: %v", err)
+	//	return
+	//}
+
+    consoleUrl := os.Getenv("PRISMACLOUDCOMPUTE_CONSOLE_URL") 
+    username := os.Getenv("PRISMACLOUDCOMPUTE_USERNAME")
+    password := os.Getenv("PRISMACLOUDCOMPUTE_PASSWORD")
+    insecure := true
+    requestTimeout := 60
+
+    config := api.PrismaCloudComputeAPIClientConfig{
+        ConsoleURL: &consoleUrl,
+        Username: &username,
+        Password: &password,
+        Insecure: &insecure,
+        RequestTimeout: &requestTimeout,
+    }
 
     // Create client, initializing authentication workflow
-	client, err = api.Client(config)
+    client, err := api.Client(config)
 	if err != nil {
 		t.Errorf("Error initializing API client: %v", err)
 		return
