440ec03e40d7afd744c4ec4c6b0b083272f04d57
Max Boynton <mboynton@paloaltonetworks.com>
2025-06-17 11:49:40 -0500

    add helper funcs for logging structs

--- internal/util/logging.go
+++ internal/util/logging.go
@@ -9,34 +9,40 @@ import (
 )
 
 var (
-    Log *log.Logger
-    debugLogPaddingEnvVar string = "PRISMACLOUDCOMPUTE_DEBUG_LOG_PADDING"
-    debugLogPadding string = ""
-    debugLogPaddingEnabled bool
+	Log                    *log.Logger
+	debugLogPaddingEnvVar  string = "PRISMACLOUDCOMPUTE_DEBUG_LOG_PADDING"
+	debugLogPadding        string = ""
+	debugLogPaddingEnabled bool
 )
 
 func init() {
-    // Assign address of default logger to Log
+	// Assign address of default logger to Log
 	Log = log.Default()
 
-    // Remove log message datetime prefix by clearing the Ldate and Ltime flags using a bit clear operaton
-    Log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+	// Remove log message datetime prefix by clearing the Ldate and Ltime flags using a bit clear operaton
+	Log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 
-    // Add newlines around log messages for easier troubleshooting if PRISMACLOUDCOMPUTE_DEBUG_LOG_PADDING env var is set to true
-    err := GetEnvironmentVariable(debugLogPaddingEnvVar, &debugLogPaddingEnabled)
-    if err != nil {
-        Log.Printf("Failed to parse %s value: %s", debugLogPaddingEnvVar, err.Error())
-    } else if (debugLogPaddingEnabled == true) {
-        debugLogPadding = "\n\n" 
-    }
+	// Add newlines around log messages for easier troubleshooting if PRISMACLOUDCOMPUTE_DEBUG_LOG_PADDING env var is set to true
+	err := GetEnvironmentVariable(debugLogPaddingEnvVar, &debugLogPaddingEnabled)
+	if err != nil {
+		Log.Printf("Failed to parse %s value: %s", debugLogPaddingEnvVar, err.Error())
+	} else if debugLogPaddingEnabled == true {
+		debugLogPadding = "\n\n"
+	}
+	// TESTING:
+	debugLogPadding = "\n\n"
 }
 
 func LogDebug(message string) {
-    Log.Printf(fmt.Sprintf("%s%s%s\n", debugLogPadding, message, debugLogPadding))
+	Log.Printf(fmt.Sprintf("%s%s%s\n", debugLogPadding, message, debugLogPadding))
 }
 
 func LogfDebug(object interface{}) {
-    Log.Printf(fmt.Sprintf("%s%s%s\n", debugLogPadding, object, debugLogPadding))
+	Log.Printf(fmt.Sprintf("%s%s%s\n", debugLogPadding, object, debugLogPadding))
+}
+
+func LogDebugStruct(object interface{}) {
+	Log.Printf(fmt.Sprintf("%s%+v%s\n", debugLogPadding, object, debugLogPadding))
 }
 
 func HCLogInfo(ctx context.Context, message string) {
